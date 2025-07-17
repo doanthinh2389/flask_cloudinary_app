cfcb743fb9a01e06af946856fc45799a76480bef
Doan Quoc Thinh <doanquocthinh2389@gmail.com>
2025-07-18 01:45:50 +0700

    Fix host and port for Render deployment

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def index():
     return render_template('upload.html', gallery=gallery)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
